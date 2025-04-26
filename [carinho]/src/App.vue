69932bd6efb026e182fb70cff2e0ca830db47f2c
Andersonramos2023 <andersonrsanjo@gmail.com>
2025-04-26 20:48:38 -0300

    first commit

---
+++ [carinho]/src/App.vue
@@ -0,0 +1,66 @@
+
+<template>
+  <!DOCTYPE html>
+  <html lang="pt-br">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cabeçalho Exemplo</title>
+  </head>
+  <body>
+
+    <header class="header">
+
+      <div class="logo">
+        <h1>Mutiverso comics</h1>
+      </div>
+
+        <p class="pesquisa"> Pesquisar comics?</p>
+
+      <nav class="navegacao">
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Sobre</a></li>
+          <li><a href="#">Serviços</a></li>
+          <li><a href="#">Contato</a></li>
+          <li><a href="#">Login</a></li>
+
+           <div class="divisao"> <img src="/public/img/Line 2.png" alt="linha"> </div>
+
+          <li><a href="#">Cadastro</a></li>
+
+          <div class="divisao2"> <img src="/public/img/" alt="linha"> </div>
+
+          <li><a href="#">carinho</a></li>
+        </ul>
+      </nav>
+    </header>
+
+    <div class="linha"></div>
+
+    <main>
+
+      <section class="apresentacao">
+
+
+          <h2>Bem-vindo ao Mutiverso Comics</h2>
+        <div>
+          <a href="#">Altor da marvel</a>
+        </div>
+
+          <h1>Stanley Martin Lieber</h1>
+
+          <p>O principal autor e editor da Marvel Comics foi Stan Lee (Stanley Martin Lieber), um escritor, editor, produtor e ator norte-americano. Ele é conhecido por ter co-criado muitos dos personagens icônicos da Marvel Comics, como o Homem-Aranha, os X-Men, o Hulk, Thor, o Homem de Ferro e o Quarteto Fantástico. </p>
+
+          <div class="botao"><a href="#">Ler página</a></div>
+
+          <!-- //////// FALTA COLOCAR A IMG ///////-->
+          <div class="linha3"></div>
+        </section>
+        <section>
+          aaaaaaaaaaaaaa
+        </section>
+    </main>
+  </body>
+  </html>
+  </template>
