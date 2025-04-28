0ea7d36185f627421eea13e100918908a8d8d0b5
Anderson Ramos Silva Anjo <163339790+Andersonramos2023@users.noreply.github.com>
2025-04-28 18:15:08 -0300

    MultiversoComics.vue

--- [carinho]/src/App.vue
+++ [carinho]/src/App.vue
@@ -1,12 +1,14 @@
-
 <template>
+
   <!DOCTYPE html>
   <html lang="pt-br">
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/main.css">
     <title>Cabeçalho Exemplo</title>
   </head>
+
   <body>
 
     <header class="header">
@@ -15,7 +17,7 @@
         <h1>Mutiverso comics</h1>
       </div>
 
-        <p class="pesquisa"> Pesquisar comics?</p>
+      <p class="pesquisa"> Pesquisar comics?</p>
 
       <nav class="navegacao">
         <ul>
@@ -25,12 +27,12 @@
           <li><a href="#">Contato</a></li>
           <li><a href="#">Login</a></li>
 
-           <div class="divisao"> <img src="/public/img/Line 2.png" alt="linha"> </div>
+         <div class="divisao">  </div>
 
           <li><a href="#">Cadastro</a></li>
 
-          <div class="divisao2"> <img src="/public/img/" alt="linha"> </div>
-
+          <div class="divisao">  </div>
+          
           <li><a href="#">carinho</a></li>
         </ul>
       </nav>
@@ -43,24 +45,67 @@
       <section class="apresentacao">
 
 
-          <h2>Bem-vindo ao Mutiverso Comics</h2>
+        <h2>Bem-vindo ao Mutiverso Comics</h2>
         <div>
-          <a href="#">Altor da marvel</a>
+          <a href="#">Autor da marvel</a>
         </div>
 
-          <h1>Stanley Martin Lieber</h1>
+        <h1>Stanley Martin Lieber</h1>
 
-          <p>O principal autor e editor da Marvel Comics foi Stan Lee (Stanley Martin Lieber), um escritor, editor, produtor e ator norte-americano. Ele é conhecido por ter co-criado muitos dos personagens icônicos da Marvel Comics, como o Homem-Aranha, os X-Men, o Hulk, Thor, o Homem de Ferro e o Quarteto Fantástico. </p>
+        <p>O principal autor e editor da Marvel Comics foi Stan Lee (Stanley Martin Lieber), um escritor, editor,
+          produtor e ator norte-americano. Ele é conhecido por ter co-criado muitos dos personagens icônicos da Marvel
+          Comics, como o Homem-Aranha, os X-Men, o Hulk, Thor, o Homem de Ferro e o Quarteto Fantástico. </p>
 
-          <div class="botao"><a href="#">Ler página</a></div>
+        <div class="botao"><a href="#">Acessar página do autor</a></div>
 
-          <!-- //////// FALTA COLOCAR A IMG ///////-->
-          <div class="linha3"></div>
+        <!-- //////// FALTA COLOCAR A IMG ///////-->
+      </section>
+      <div class="linha3"></div>
+
+      <section class="indicacao">
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <a href="#">Frete grátis para SC</a>
+              </li>
+
+              <div class="reverce"></div>
+
+              <li>
+                <a href="#">Livros recomendados</a>
+              </li>
+
+              <div class="reverce"></div>
+
+              <li>
+                <a href="#">Mais vendidos</a>
+
+                <div class="sublinhado"></div>
+
+              </li>
+            </ul>
+          </nav>
+        </div>
+
+      </section>
+
+      <div class="linha3"></div>
+        <section class="comics">
+          <div class="quadrinhos">
+
+        
+         
+        
+          </div>
         </section>
-        <section>
-          aaaaaaaaaaaaaa
-        </section>
+        <footer>
+
+        </footer>
     </main>
   </body>
+
   </html>
-  </template>
+</template>
+
+
